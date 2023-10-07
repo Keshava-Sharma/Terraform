77ccfbd82112ba2a96a2c940613d97df58382515
Keshava-Sharma <142989964+Keshava-Sharma@users.noreply.github.com>
2023-10-07 05:43:53 +0000

    Day 3

---
+++ main.tf
@@ -0,0 +1,43 @@
+/*
+
+provider "azurerm" {
+  features {
+    
+  }
+}
+
+variable "purpose" {
+  description = "What is the purpose for storage account Dev/Prod ?"
+
+  validation {
+    condition = var.purpose == "dev" || var.purpose == "prod"
+    error_message = "Input accepeted dev/prod only"
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg3"
+  location = "South India"
+}
+
+resource "azurerm_storage_account" "example1" {
+  name                     = "oct72023k7n"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  #account_replication_type = var.purpose == "dev" ? "LRS" : "GRS"
+  account_replication_type = "LRS"
+  count = var.purpose == "dev" ? 1 : 0
+}
+
+resource "azurerm_storage_account" "ex2" {
+  name                     = "oct72023k7n"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  #account_replication_type = var.purpose == "dev" ? "LRS" : "GRS"
+  account_replication_type = "GRS"
+  count = var.purpose == "prod" ? 1 : 0
+}
+
+*/
